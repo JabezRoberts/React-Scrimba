c3b36d3bb0af61997cb2f6c1614287b5eddb1658
Jabez Roberts <jabez.roberts137@gmail.com>
2025-08-28 19:34:29 -0500

    start useRef

---
+++ Advanced React/1. Reusability/3. Toggle Star/Star.js
@@ -0,0 +1,66 @@
+import React from "react"
+import { BsStar, BsStarFill } from "react-icons/bs"
+
+export default function Star() {
+    const [starred, setStarred] = React.useState(false)
+    
+    function toggle() {
+        setStarred(prev => !prev)
+    }
+    
+    return (
+        starred ? 
+        <BsStarFill className="star filled" onClick={toggle} /> :
+        <BsStar className="star " onClick={toggle} />
+    )
+}
+
+
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  const [text, setText] = React.useState("")
+  const [list, setList] = React.useState([])
+  const inputRef = React.useRef(null)
+
+  function handleChange(e) {
+    setText(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!text) {
+      return;
+    }
+    setList(prevList => [...prevList, text])
+    setText("")
+    inputRef.current.focus()
+    
+  }
+  
+  return (
+    <>
+      <h2>React Project Ideas</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={text}
+          placeholder="Idea"
+          ref={inputRef}
+        />
+        <button>Submit</button>
+      </form>
+
+      <ol>
+        {list.map((item, i) => <li key={i}>{item}</li>)}
+      </ol>
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
